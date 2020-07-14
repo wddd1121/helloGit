568c27583e3d53d8ab42f3fd9f86d501ce18c1ea
yh_glb <862815933_glb@qq.com>
2020-07-15 07:36:40 +0800

    更新2

--- helloGit/src/main/java/com/yh/HelloGit.java
+++ helloGit/src/main/java/com/yh/HelloGit.java
@@ -5,5 +5,6 @@ public class HelloGit {
     public static void main(String[] args) {
         System.out.println("hello git");
         System.out.println("update 1");
+        System.out.println("update 2");
     }
 }
